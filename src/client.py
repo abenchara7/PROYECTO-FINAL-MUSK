61167d765163352a902ea551b8f470f033294b37
abenchara7 <abenchara7@gmail.com>
2026-06-09 09:07:19 +0200

    limpio mensajes innecesarios y mejoro la legibilidad

--- src/client.py
+++ src/client.py
@@ -1,12 +1,12 @@
 class Client:
-    # Constructor: recibe los 4 datos del cliente
+    
     def __init__(self, client_id, name, country, signup_date):
         self.client_id = client_id
         self.name = name
         self.country = country
         self.signup_date = signup_date
 
-    # Convierte el objeto a diccionario (útil para el reporte final)
+    
     def to_dict(self):
         return {
             "client_id": self.client_id,
--- src/sale.py
+++ src/sale.py
@@ -1,5 +1,5 @@
 class Sale:
-    # Constructor: recibe los 6 datos de la venta
+    
     def __init__(self, sale_id, client_id, product, category, amount, date):
         self.sale_id = sale_id
         self.client_id = client_id
@@ -8,7 +8,7 @@ class Sale:
         self.amount = amount
         self.date = date
 
-    # Convierte el objeto a diccionario (útil para el reporte final)
+    
     def to_dict(self):
         return {
             "sale_id": self.sale_id,
